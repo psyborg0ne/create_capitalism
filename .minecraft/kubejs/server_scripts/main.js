a5ee956bb930b6802f6514c75bfc0c25df6d677a
Orfeas Artinopoulos <artinopoulos.orfeas@gmail.com>
2026-03-30 19:26:28 +0300

    Initial config

--- .minecraft/kubejs/server_scripts/main.js
+++ .minecraft/kubejs/server_scripts/main.js
@@ -1,3 +1,6 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded server example script)')
-
+BlockEvents.rightClicked('kubejs:vending_machine', event => {
+    // event.server.runCommand("/hqs shop open")
+    event.player.runCommand("/hqs shop open")
+});
--- .minecraft/kubejs/startup_scripts/main.js
+++ .minecraft/kubejs/startup_scripts/main.js
@@ -1,3 +1,6 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded startup example script)')
 
+StartupEvents.registry('block', event => {
+    event.create('vending_machine').displayName("Vending Machine")
+})
